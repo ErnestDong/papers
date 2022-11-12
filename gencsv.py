ee96efa45d19c311c0598117140e2460c9539326
Ernest Dong <dongcy2000@gmail.com>
2022-11-12 12:02:33 +0800

    init for risk management

--- gencsv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-excel = pd.ExcelFile('data/data.xlsx')
-for sheet in excel.sheet_names:
-    if "raw" in sheet:
-        continue
-    df = excel.parse(sheet)
-    df.to_csv('data/%s.csv' % sheet, index=False, float_format='%.3f')
