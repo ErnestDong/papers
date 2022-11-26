7bd67f987a3aed84bb0b1fb7276d04ae5859e1dc
Ernest Dong <dongcy2000@gmail.com>
2022-11-26 18:37:22 +0800

    风管论文
    Squashed commit of the following:
    
    commit 4bf8aedde1fc5989ade8521dc20b27a48e1109b2
    Author: Ernest Dong <dongcy2000@gmail.com>
    Date:   Sat Nov 26 18:32:50 2022 +0800
    
        完成文字稿
    
    commit b81ad0c9d042df81f192d1bcda8c75aa9d5a6e46
    Author: Ernest Dong <dongcy2000@gmail.com>
    Date:   Fri Nov 25 15:43:31 2022 +0800
    
        update
    
    commit db6fd3ee908f693129f56659d8bc2669cf6bce07
    Author: Ernest Dong <dongcy2000@gmail.com>
    Date:   Thu Nov 24 10:56:40 2022 +0800
    
        attach data
    
    commit cc7a0033d2fcc8bbdf5ea17b6bcc531d5bd4af7a
    Author: Ernest Dong <dongcy2000@gmail.com>
    Date:   Wed Nov 23 22:08:32 2022 +0800
    
        完成数据部分
    
    commit 055da36bcd04cad65373f6173a94cf913ff5c697
    Author: Ernest Dong <dongcy2000@gmail.com>
    Date:   Wed Nov 23 16:34:31 2022 +0800
    
        update
    
    commit b5d443043f58f84be8d40bc41d189663c2f459bf
    Author: Ernest Dong <dongcy2000@gmail.com>
    Date:   Mon Nov 21 14:28:06 2022 +0800
    
        update
    
    commit 332182efd1f4c7b8efc540369404cc3287a8441b
    Author: Ernest Dong <dongcy2000@gmail.com>
    Date:   Sun Nov 13 22:31:22 2022 +0800
    
        风险管理pre ppt
    
    commit e04f2a873e3e35ae10cf60bd7fe92593323509c3
    Author: Ernest Dong <dongcy2000@gmail.com>
    Date:   Sun Nov 13 22:29:30 2022 +0800
    
        风险管理pre文字稿
    
    commit 260a24d603200be7015629100bcc9473efab1ade
    Author: Ernest Dong <dongcy2000@gmail.com>
    Date:   Sun Nov 13 19:53:07 2022 +0800
    
        文本部分半完成
    
    commit f72159e150539f8bfe61241f0e756f71888bd239
    Author: Ernest Dong <dongcy2000@gmail.com>
    Date:   Sun Nov 13 16:21:47 2022 +0800
    
        update
    
    commit 6011dd1e2a160908a46729d38964499a9805a99f
    Author: Ernest Dong <dongcy2000@gmail.com>
    Date:   Sun Nov 13 12:31:53 2022 +0800
    
        update
    
    commit defe84bd4210d1481a423a2d8a5d437cd57da5e7
    Author: Ernest Dong <dongcy2000@gmail.com>
    Date:   Sun Nov 13 10:43:16 2022 +0800
    
        update
    
        唉
    
    commit ee96efa45d19c311c0598117140e2460c9539326
    Author: Ernest Dong <dongcy2000@gmail.com>
    Date:   Sat Nov 12 12:02:33 2022 +0800
    
        init for risk management

---
+++ src/debt.py
@@ -0,0 +1,35 @@
+#%%
+#!%cd /Users/dcy/Code/papers.worktrees/risk-management
+import pandas as pd
+
+st = pd.read_excel("data/实施ST.xlsx", skipfooter=2)
+unst = pd.read_excel("data/撤消ST.xlsx", skipfooter=2)
+unst = unst[unst["撤销日期"].dt.year < 2008]
+st = st[st["实施日期"].dt.year < 2008]
+# %%
+st["type"] = "st"
+unst["type"] = "unst"
+st["date"] = st["实施日期"]
+unst["date"] = unst["撤销日期"]
+st["name_before"] = st["实施前简称"]
+st["name_after"] = st["实施后简称"]
+unst["name_before"] = unst["撤销前简称"]
+unst["name_after"] = unst["撤销后简称"]
+df = pd.concat([st, unst])[["代码", "date", "name_before", "name_after", "type"]]
+df.sort_values("date", inplace=True)
+
+# %%
+result = []
+for stock, stockdf in df.groupby("代码"):
+    result.append(
+        {
+            "stock": stock,
+            "name": stockdf["name_after"].values[-1],
+            "type": stockdf["type"].values[-1],
+            "date": stockdf["date"].values[-1],
+        }
+    )
+result = pd.DataFrame(result).dropna()
+# %%
+result[result["name"].str.contains("ST")].to_excel("data/sts.xlsx")
+# %%
---
+++ src/kmv.py
@@ -0,0 +1,77 @@
+#%%
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from dataloader import Dataloader
+
+project_root = Path(__file__).parent.parent
+sns.set(
+    rc={"font.family": "SimHei", "figure.figsize": (6, 3), "axes.unicode_minus": False},
+    style="white",
+)
+
+
+#!%cd /Users/dcy/Code/papers.worktrees/risk-management
+class KMV:
+    """利用KMV模型计算违约距离"""
+
+    def __init__(
+        self, filename: str = Path(__file__).parent.parent / "data/KMV模型已知量汇总.xlsx"
+    ) -> None:
+        with pd.ExcelFile(filename) as f:
+            self.sigma_e = Dataloader("sigma_e", f).data
+            self.sigma_a = Dataloader("sigma_a", f).data
+            self.v_e = Dataloader("v_e", f).data
+            self.v_a = Dataloader("v_a", f).data
+            self.r = Dataloader("r", f).data / 100
+            self.d, self.label = (tmp := Dataloader("d", f)).data, tmp.label
+
+    def distance_to_default(self) -> pd.DataFrame:
+        """计算违约距离
+
+        Returns:
+            pd.DataFrame: 公司x日期的违约距离
+        """
+        correct = self._filter()
+        sigma_a = self.sigma_a[correct]
+        v_a = self.v_a[correct]
+        d = self.d[correct]
+        r = self.r[correct]
+        t = 252
+        return (np.log(v_a / d) + (r - 0.5 * sigma_a**2 * t)) / (sigma_a * t**0.5)
+
+    def _filter(self):
+        """删除股权波动率为0的股票
+
+        Returns:
+            list[str]: 股权价值波动率为正的股票
+        """
+        correct_stock = [i for i in self.sigma_e.columns if self.sigma_e[i].prod() != 0]
+        return correct_stock
+
+
+if __name__ == "__main__":
+    model = KMV()
+    dd = model.distance_to_default()
+    res = dd.stack().reset_index()
+    res.columns = ["date", "Stock", "DD"]
+    st = (
+        pd.read_excel(
+            project_root / "data/sts.xlsx", usecols=["stock", "name", "date", "enddate"]
+        )
+        .set_index("stock")
+        .to_dict()
+    )
+    res["ST"] = res.apply(
+        lambda x: "ST"
+        if (temp := model.label[x["Stock"]]) in st["name"]
+        and st["date"][temp] <= x["date"] <= st["enddate"][temp]
+        else "非ST",
+        axis=1,
+    )
+    res["date"] = res["date"].dt.strftime("%y-%m")
+    fig = sns.scatterplot(data=res, x="date", y="DD").get_figure()
+    fig.savefig(project_root / "img/dd.png")
+# %%
